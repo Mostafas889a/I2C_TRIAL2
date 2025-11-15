3894eee44d7c1e72d7019fa6662ddd784f715c90
Mostafa <rady@nativechips.ai>
2025-11-15 07:09:23 -0800

    "autosyncing agent activities"

--- verilog/dv/cocotb/sim/i2c_test/RTL-i2c_test/rerun.py
+++ verilog/dv/cocotb/sim/i2c_test/RTL-i2c_test/rerun.py
@@ -12,7 +12,7 @@ os.environ["MCW_ROOT"] = "/nc/templates/mgmt_core_wrapper"
 
 os.chdir("/workspace/I2C_TRIAL2/verilog/dv/cocotb")
 
-command = "python3 /usr/local/bin/caravel_cocotb -test i2c_test -tag i2c_test/RTL-i2c_test/rerun   -sim RTL -corner nom-t  -seed 1763219079 "
+command = "python3 /usr/local/bin/caravel_cocotb -test i2c_test -tag i2c_test/RTL-i2c_test/rerun   -sim RTL -corner nom-t  -seed 1763219307 "
 if args.extend is not None:
     command += f" {args.extend}"
 os.system(command)
