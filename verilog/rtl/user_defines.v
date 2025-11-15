3d52881a6552c61e8e07281bfd1942d4a560c3b7
Mostafa <rady@nativechips.ai>
2025-11-15 06:01:47 -0800

    "autosyncing agent activities"

--- verilog/rtl/user_defines.v
+++ verilog/rtl/user_defines.v
@@ -90,7 +90,8 @@
 `define USER_CONFIG_GPIO_37_INIT `GPIO_MODE_INVALID
 `define USE_USER_VIP
 
-assign  sda_pin = ~i2c_sda_o ? 1'b0 : 1'bz;
+tri1  sda_pin = ~i2c_sda_o ? 1'b0 : 1'bz;
+tri1  scl_pin = ~i2c_scl_o ? 1'b0 : 1'bz;
 
 `define USER_VIP M24AA64 slave(.A0(1), .A1(0), .A1(1), .WP(0), .SDA(), .SCL(), .RESET())
 
