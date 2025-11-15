fe76b18a6dc4e2a4bbc836245f77e0518c243a98
Mostafa <rady@nativechips.ai>
2025-11-15 06:30:22 -0800

    "autosyncing agent activities"

--- verilog/dv/cocotb/i2c_test/i2c_test.py
+++ verilog/dv/cocotb/i2c_test/i2c_test.py
@@ -3,7 +3,7 @@ from caravel_cocotb.caravel_interfaces import test_configure, report_test
 from cocotb.triggers import RisingEdge, FallingEdge
 import sys
 sys.path.append('..')
-from VirtualGPIOModel import VirtualGPIOModel
+#from VirtualGPIOModel import VirtualGPIOModel
 
 @cocotb.test()
 async def i2c_test(dut):
