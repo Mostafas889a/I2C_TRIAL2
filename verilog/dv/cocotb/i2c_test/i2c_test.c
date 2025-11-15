2dc5ffa083ae0a0926a1a0fc8d467d8c2ac1a854
Mostafa <rady@nativechips.ai>
2025-11-15 07:22:29 -0800

    "autosyncing agent activities"

--- verilog/dv/cocotb/i2c_test/i2c_test.c
+++ verilog/dv/cocotb/i2c_test/i2c_test.c
@@ -33,45 +33,6 @@ void main() {
     GPIOs_loadConfigs();
     User_enableIF();  
     vgpio_write_output(1);
-
-    i2c_init(62);
-    vgpio_write_output(2);
-    
-    
-    i2c_write_byte(I2C_SLAVE_ADDR, 0, 0xAA);
-    i2c_write_byte(I2C_SLAVE_ADDR, 1, 0x55);
-    i2c_write_byte(I2C_SLAVE_ADDR, 2, 0xDE);
-    i2c_write_byte(I2C_SLAVE_ADDR, 3, 0xAD);
-
-    
-    uint8_t val0 = i2c_read_byte(I2C_SLAVE_ADDR, 0);
-    uint8_t val1 = i2c_read_byte(I2C_SLAVE_ADDR, 1);
-    uint8_t val2 = i2c_read_byte(I2C_SLAVE_ADDR, 2);
-    uint8_t val3 = i2c_read_byte(I2C_SLAVE_ADDR, 3);
-    
-    
-    if (val0 == 0xAA && val1 == 0x55 && val2 == 0xDE && val3 == 0xAD) {
-        ManagmentGpio_write(1);
-    } else {
-        return;
-    }
-    
-    
-    i2c_eeprom_write(I2C_EEPROM_ADDR, 0x0000, 0x12);
-    i2c_eeprom_write(I2C_EEPROM_ADDR, 0x0001, 0x34);
-    i2c_eeprom_write(I2C_EEPROM_ADDR, 0x0010, 0x56);
-    i2c_eeprom_write(I2C_EEPROM_ADDR, 0x0100, 0x78);
-    
-    
-    uint8_t eeprom_val0 = i2c_eeprom_read(I2C_EEPROM_ADDR, 0x0000);
-    uint8_t eeprom_val1 = i2c_eeprom_read(I2C_EEPROM_ADDR, 0x0001);
-    uint8_t eeprom_val2 = i2c_eeprom_read(I2C_EEPROM_ADDR, 0x0010);
-    uint8_t eeprom_val3 = i2c_eeprom_read(I2C_EEPROM_ADDR, 0x0100);
-    
-    
-    if (eeprom_val0 == 0x12 && eeprom_val1 == 0x34 && eeprom_val2 == 0x56 && eeprom_val3 == 0x78) {
-        ManagmentGpio_write(1);
-    }
-    
+    CF_I2C_setGclkEnable()
     return;
 }
