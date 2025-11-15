ee024484002c8f989a393b2456328706c92413c7
Mostafa <rady@nativechips.ai>
2025-11-15 08:03:51 -0800

    "autosyncing agent activities"

--- verilog/dv/cocotb/i2c_test/i2c_test.py
+++ verilog/dv/cocotb/i2c_test/i2c_test.py
@@ -9,14 +9,14 @@ from i2c_slave import i2c_slave
 @report_test
 async def i2c_test(dut):
     caravelEnv = await test_configure(dut, timeout_cycles=561831)
-    caravelEnv.drive_gpio_in((9,8),0)
     cocotb.log.info("[TEST] Starting I2C master-slave communication test")
     cocotb.log.info("[TEST] Firmware will test both simple slave and M24AA64 EEPROM")
     
     virtual_gpio = VirtualGPIOModel(caravelEnv)
     virtual_gpio.start()
-    await cocotb.start(i2c_slave(caravalEnv.dut.gpio8_monitor, caravalEnv.dut.gpio9_monitor, caravalEnv.dut.gpio9), {0x2,0x4,0x6,0x18})
 
     await virtual_gpio.wait_output(1)
+    caravelEnv.drive_gpio_in((9),0)
+    await cocotb.start(i2c_slave(caravelEnv.dut.gpio8_monitor, caravelEnv.dut.gpio9_monitor, caravelEnv.dut.gpio9), {0x2,0x4,0x6,0x18})
     await virtual_gpio.wait_output(2)
 
